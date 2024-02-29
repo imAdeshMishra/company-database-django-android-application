51a517dc78ef57c8717b2ec7309136372b8f283d
Adesh Mishra <mishradesh01@gmail.com>
2024-03-01 02:01:06 +0530

    test commit

--- SimpleDjangoApp/app/src/main/java/com/example/simpledjangoapp/RetrofitClient.kt
+++ SimpleDjangoApp/app/src/main/java/com/example/simpledjangoapp/RetrofitClient.kt
@@ -4,9 +4,8 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://192.168.101.8:8000/"
-//    private const val BASE_URL = "http://127.0.0.1/:8000/"
-//    private const val BASE_URL = "http://10.0.2.2:8000/"
+    private const val BASE_URL = "http://192.168.101.8:8000/" // for real android device
+//    private const val BASE_URL = "http://10.0.2.2:8000/" //for emulator
 
     private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
